72ed3a8b026f8801cfe9a46f5f8762dd515beda5
Philippe Charrière <ph.charriere@gmail.com>
2022-10-30 06:34:01 +0000

    :memo: :memo: Add or update documentation. save

--- git.sh
+++ git.sh
@@ -1,69 +1,28 @@
 #!/bin/bash
 
 # https://gitmoji.dev/
-message="updated"
+# https://linuxize.com/post/bash-case-statement/
 
-if [[ "$1" == "art" ]]; then
-  message=":art: Improve structure / format of the code. $2"
-fi
+message=""
 
-if [[ "$1" == "zap" ]]; then
-  message=":zap: Improve performance. $2"
-fi
+case $1 in
 
-if [[ "$1" == "memo" ]]; then
-  message=":memo: Add or update documentation. $2"
-fi
+  art)
+    message="Improve structure / format of the code. $2"
+    ;;
+
+  zap)
+    message="Improve performance. $2"
+    ;;
+
+  memo)
+    message=":memo: Add or update documentation. $2"
+    ;;
+
+  *)
+    message="updated. $2"
+    ;;
+esac
 
 
-
-if [[ "$1" == "wip" ]]; then
-  message="🚧 WIP $2"
-fi
-
-if [[ "$1" == "sample" ]]; then
-  message="🏓 sample $2"
-fi
-
-
-if [[ "$1" == "init" ]]; then
-  message="🎉 first commit $2"
-fi
-
-if [[ "$1" == "save" ]]; then
-  message="📝 save $2"
-fi
-
-if [[ "$1" == "update" ]]; then
-  message="📝 update $2"
-fi
-
-if [[ "$1" == "gardening" ]]; then
-  message="🌺 gardening $2"
-fi
-
-if [[ "$1" == "feature" ]]; then
-  message="✨ feature $2"
-fi
-
-if [[ "$1" == "doc" ]]; then
-  message="📝 update doc $2"
-fi
-
-if [[ "$1" == "fix" ]]; then
-  message="🐛 fix $2"
-fi
-
-if [[ "$1" == "config" ]]; then
-  message="🔧 config $2"
-fi
-
-if [[ "$1" == "docker" ]]; then
-  message="🐳 docker $2"
-fi
-
-if [[ "$1" == "release" ]]; then
-  message="📦 release $2"
-fi
-
-git add .; git commit -m "${message}"; git push
+git add .; git commit -m ":${1}: ${message}"; git push
